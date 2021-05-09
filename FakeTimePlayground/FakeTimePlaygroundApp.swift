df4614e50415f06553c9394b58d8ef200df63fd9
Ammaar Reshi <ammaar@me.com>
2021-05-09 16:17:10 +0100

    Initial Commit

---
+++ FakeTimePlayground/FakeTimePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeTimePlaygroundApp.swift
+//  FakeTimePlayground
+//
+//  Created by Ammaar Reshi on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FakeTimePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
